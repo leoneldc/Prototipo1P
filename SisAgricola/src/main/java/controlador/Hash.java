ceb6dab460ab05fffdfa41c21d813b46f68c7cfe
leoneldc <leonelchavez2013@hotmail.com>
2022-08-09 11:43:57 -0600

    Creación de contenedor con sistema de login

---
+++ SisAgricola/src/main/java/controlador/Hash.java
@@ -0,0 +1,19 @@
+package controlador;
+
+import java.util.Base64;
+
+public class Hash {
+    // Retorna un hash MD5 a partir de un texto 
+    public static String getMD5(String txt) {
+        Base64.Encoder encoder = Base64.getEncoder();
+        String encodedString = encoder.encodeToString(txt.getBytes());
+        return encodedString;
+    }
+ 
+    // Retorna un string a partir de un MD5
+    public static String getString(String txt) {
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] bytes = decoder.decode(txt);
+        return new String(bytes);
+    }
+}
